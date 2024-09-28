65af2950b59341061113ccbfe633ddd0d75b9a6c
arsenalmorimori <arsenalbautista1@gmail.com>
2024-09-28 22:01:55 +0800

    dashboard added

--- src/Components/Card_About/Card.jsx
+++ src/Components/Card_About/Card.jsx
@@ -5,25 +5,25 @@ function Card() {
   return (
     <div>
       <main id="cards" class="cards">
-            <div class="card">
+            <div class="card card1">
               <h2> </h2>
               <p>
               
               </p>
             </div>
-            <div class="card">
+            <div class="card card2">
               <h2> </h2>
               <p>
               
               </p>
             </div>
-            <div class="card">
+            <div class="card card3">
               <h2> </h2>
               <p>
               
               </p>
             </div>
-            <div class="card">
+            <div class="card card4">
               <h2> </h2>
               <p>
               
